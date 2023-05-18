766332c2ded2b1305f6fac2cf111bcee4ca41aa7
Rodnei Dias <rodnei.dias4@gmail.com>
2023-05-18 20:15:04 -0300

    Criação da página de descrição

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -14,6 +14,14 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
+    pokemon.height =pokeDetail.height
+    pokemon.weight = pokeDetail.weight
+    
+    const abilities = pokeDetail.abilities.map((abilitiesSlot) => abilitiesSlot.ability.name)
+    const [abilitie] = abilities
+    pokemon.abilities = pokeDetail.abilities
+    pokemon.ability = abilitie
+
     return pokemon
 }
 
@@ -23,7 +31,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = pokemon.number, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
@@ -33,3 +41,14 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+// pokeApi.getPokemonsDetails = (pokemon, limit = 5) => {
+//     const url = `https://pokeapi.co/api/v2/pokemon?offset=${pokemon.number}&limit=${limit}`
+
+//     return fetch(url)
+//         .then((response) => response.json())
+//         .then((jsonBody) => jsonBody.results)
+//         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+//         .then((detailRequests) => Promise.all(detailRequests))
+//         .then((pokemonsDetails) => pokemonsDetails)
+// }
---
+++ assets/js/pokemon-description-main.js
@@ -0,0 +1,94 @@
+
+const card = document.getElementById('card')
+
+const maxRecords = 151
+const limit = 1;
+let offset = 5
+
+function convertPokemonCard(pokemon){
+    return `
+    
+        <header class="icons">
+            <span><a href="../index.html"><i class="fa-solid fa-arrow-left" ></i></a></span>
+           <span><i class="heart fa-sharp fa-regular fa-heart" ></i></span> 
+        </header>
+   
+        <div class="focus-in-contract-bck">
+            <h1 class="pad-0-2">${pokemon.name}</h1>
+        </div>
+     
+        <div class="content-display-type">
+            <div class="box-display-type">
+            ${pokemon.types.map((type) => `<li class="type display-type ${type}">${type}</li>`).join('')}
+                
+            </div>
+            <div>
+                <span class="types">#${pokemon.number}</span>
+            </div>            
+        </div>
+      
+        <div class="content-photo animation-span">
+            <img class="photo" src="${pokemon.photo}" alt="${pokemon.name}" srcset="">
+        </div>
+     
+        <div class=" content-bottom">
+               
+             <div class="content-bottom-list-01 ">
+                 <span>About</span>
+                 <span>Base Status</span>
+                 <span>Evolution</span>
+                 <span>Moves</span>
+             </div>
+        
+            <div class="content-bottom-list-02 ">
+            
+                <div class="content-coll-01">
+                    <span>Species</span>
+                    <span>Height</span>
+                    <span>weight</span>
+                    <span>Abilities</span>
+                </div>
+          
+                <div class="content-coll-02 ">
+                    <span>${pokemon.species}</span>
+                    <span>2' 3,6* (${pokemon.height} cm) </span>
+                    <span>15,2 lbs (${pokemon.weight} Kg)</span>
+                    <span>${pokemon.ability}</span>
+                </div>
+            </div>            
+          
+            <h3 class="sub-title">Breeding</h3>
+
+            <div class="content-bottom-list-03">
+                <div class="content-coll-03">
+                    <span>Gender</span>
+                    <span>Egg Groups</span>
+                    <span>Egg Cycle</span>
+                </div>
+                <div class="content-coll-04">
+                   <div class="content-row-coll">
+                        <div><i class="fa-solid fa-mars"></i> 87,5%</div>
+                        <div><i class="fa-solid fa-mercury"></i> 12,5%</div>
+                   </div> 
+                   <div class="content-coll">
+                        <span>Monster</span>
+                        <span>Grass</span>
+                   </div>                    
+                    
+                </div>
+            </div>
+        </div>      
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons) => {
+        const newHtml = pokemons.map(convertPokemonCard).join('')
+        card.innerHTML += newHtml 
+    })
+   
+}
+
+loadPokemonItens(offset, limit)
+
+
